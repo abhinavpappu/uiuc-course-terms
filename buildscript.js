d8c90d4dd5994488b2ef3587443dc935cbccb622
Abhinav Pappu <abhi1pappu9@gmail.com>
2021-04-23 18:57:40 -0500

    Set up svelte and esbuild

---
+++ buildscript.js
@@ -0,0 +1,25 @@
+// https://github.com/EMH333/esbuild-svelte
+// https://github.com/EMH333/esbuild-svelte/blob/main/example/buildscript.js
+const fs = require("fs");
+const esbuild = require('esbuild');
+const sveltePreprocess = require('svelte-preprocess');
+const esbuildSvelte = require('esbuild-svelte');
+
+const srcDirectory = './visualization';
+const outDirectory = './dist';
+
+esbuild.build({
+  entryPoints: [`${srcDirectory}/index.ts`],
+  bundle: true,
+  watch: process.argv.includes('--watch'),
+  outdir: outDirectory,
+  plugins: [
+    esbuildSvelte({ preprocess: sveltePreprocess() })
+  ],
+}).then(() => {
+  fs.copyFile(`${srcDirectory}/index.html`, `${outDirectory}/index.html`, (err) => { if (err) throw err; });
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
+
